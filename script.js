7101f019c94534be182ee06098901b80bf7d7c5d
schema-andrare <104255266+schema-andrare@users.noreply.github.com>
2024-03-11 13:31:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -172,7 +172,7 @@ function loadChoices() {
         console.log(data.currentWeek)
       schemaaa = data
         console.log(schemaaa.updated);
-        const date = new Date(schemaaa.updated.updatedDate);
+        const date = new Date(schemaaa.updated.updatedDate * 1000);
     const timeZone = 'Europe/Bucharest'
     date.setMinutes(date.getMinutes() - 60);
     const options = { weekday: 'long', timeZone, hour: '2-digit', minute: '2-digit', hour12: false, timeZoneName: 'short' };
